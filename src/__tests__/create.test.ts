91e78954c0d4c3939afb740e03886c5e6601443c
Urento <jan@burzinski.de>
2021-04-16 13:08:56 +0200

    Added some docs to tests

--- src/__tests__/create.test.ts
+++ src/__tests__/create.test.ts
@@ -1,9 +1,11 @@
 import { createHaste } from '../index';
 
+/**
+ * For running the Tests successfully the Hastebin Server needs to be running locally on port 4001
+ */
+
 test('My Greeter', async () => {
   const content = 'this is a test';
   const r = await createHaste('http://localhost:4001', content);
   expect(await r.content).toBe(content);
 });
-
-//3i3JHZoH
--- src/__tests__/get.test.ts
+++ src/__tests__/get.test.ts
@@ -1,5 +1,9 @@
 import { getHaste } from '../index';
 
+/**
+ * For running the Tests successfully the Hastebin Server needs to be running locally on port 4001
+ */
+
 test('My Greeter', async () => {
   const content = 'this is a test';
   const r = await getHaste('http://localhost:4001', '3i3JHZoH');
