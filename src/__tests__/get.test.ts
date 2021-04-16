952b81f24cc662949120773307e0d5b5b083a9d1
Urento <58522564+Urento@users.noreply.github.com>
2021-04-16 15:21:34 +0200

    Update get.test.ts

--- src/__tests__/get.test.ts
+++ src/__tests__/get.test.ts
@@ -4,7 +4,7 @@ import { getHaste } from '../index';
  * For running the Tests successfully the Hastebin Server needs to be running locally on port 4001
  */
 
-test('My Greeter', async () => {
+test('Try to get the content of a haste', async () => {
   const content = 'this is a test';
   const r = await getHaste('http://localhost:4001', '3i3JHZoH');
   expect(await r.content).toBe(content);
